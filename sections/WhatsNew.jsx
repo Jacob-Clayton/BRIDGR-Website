9493096896f26b97229516023caa7517f85b9c6f
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-20 13:54:42 +0000

    uniquie and start steps same button bg colour

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { newFeatures } from '../constants';
 import { NewFeatures, TitleText, TypingText } from '../components';
-import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
+import { staggerContainer, fadeIn } from '../utils/motion';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
