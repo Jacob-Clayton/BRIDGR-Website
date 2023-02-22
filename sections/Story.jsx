1298c4dd5bb64c534953ac7b329f87cbf2635943
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-22 18:31:52 +0000

    whitepaper button fixed for all screen sizes

--- sections/Story.jsx
+++ sections/Story.jsx
@@ -2,7 +2,6 @@
 
 import { motion } from 'framer-motion';
 import { TitleText, TypingText } from '../components';
-import Image from 'next/image';
 
 import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
@@ -35,17 +34,16 @@ const Story = () => (
             initial="hidden"
             whileInView="show"
         >
-
                 <div className='flex flex-row items-center'>     
                     <div className='relative'>
                         <a href="https://jacobclayton.gitbook.io/bridgr-services/bridgr-collective/partner-program">
-                            <button type="button" className="flex py-4 px-6 bg-black bg-opacity-40 rounded-[32px] gap-[12px]">
+                            <button type="button" className="flex py-3 px-6 bg-black bg-opacity-40 rounded-[32px] gap-[12px]">
                                 <img
                                 src="/book.svg"
                                 alt="book"
-                                className="w-[24px] h-[24px] object-contain"
+                                className="hidden sm:block w-[20px] h-[20px] sm:w-[22px] sm:h-[22px] md:w-[23px] md:h-[23px] lg:w-[24px] lg:h-[24px] xl:w-[25px] xl:h-[25px] object-contain"
                                 />
-                                <span className="font-normal text-[16px] text-white">
+                                <span className="font-normal text-[12px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[17px] 2xl:text-[18px] text-white">
                                     Whitepaper
                                 </span>
                             </button>
@@ -53,7 +51,7 @@ const Story = () => (
                     </div>
                     <motion.p
                         variants={fadeIn('up', 'tween', 0.2, 1)}
-                        className="ml-3 text-center font-normal 2xl:text-[21px] xl:text-[19px] lg:text-[18px] md:text-[16px] sm:text-[15px] text-[13px] text-secondary-white"
+                        className="ml-5 text-center font-normal 2xl:text-[21px] xl:text-[19px] lg:text-[18px] md:text-[16px] sm:text-[15px] text-[13px] text-secondary-white"
                     >
                         Read more about all our services and business model
                     </motion.p>
