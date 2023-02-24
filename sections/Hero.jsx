a875213b5a0efe0b302b08985dc4e7fd68ec2168
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-24 00:10:52 +0000

    hero image size adjustments

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
       viewport={{ once: false, amount: 0.2 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <div className="fixed inset-x-0 top-[6%] sm:top-[8%] md:top-[8%] lg:top-[11%] xl:top-[11%] 2xl:top-[10%] text-center flex justify-center items-center flex-col absolute z-20">
+      <div className="fixed inset-x-0 top-[5%] sm:top-[6%] md:top-[8%] lg:top-[9%] xl:top-[11%] 2xl:top-[10%] text-center flex justify-center items-center flex-col absolute z-20">
         <motion.h1 
           variants={textVariant(0.6)}
           className={styles.heroHeading}
@@ -39,12 +39,12 @@ const Hero = () => {
         ref={ref}
         animate={inView ? "show" : "hidden"}
         variants={fadeIn('up', 'tween', 0.2, 1)}
-        className="relative w-full lg:-mt-[8px] md:-mt-[10px] mt-[12px]"
+        className="relative w-full"
       >
         <Image
           src="/cover.png"
           alt="hero_cover"
-          className="mx-auto overflow-visible sm:mt-[6%] md:mt-[2%] lg:mt-[2%] xl:mt-[2%] 2xl:mt-[3%] mt-[7%] opacity-[90%] object-cover rounded-tl-[140px] z-10 relative"
+          className="mx-auto overflow-visible sm:mt-[16%] md:mt-[7%] lg:mt-[4%] xl:mt-[2%] 2xl:mt-[4%] mt-[25%] opacity-[90%] object-cover rounded-tl-[140px] z-10 relative"
           quality={95}
           width={1600}
           height={1000}
@@ -53,7 +53,7 @@ const Hero = () => {
         <Image 
           src='/bridgrround.png'
           alt='Bridgr logo faded in background'
-          className="absolute sm:mt-[3%] md:mt-0 mt-[5%] z-[0] w-[92%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-[19%]"
+          className="absolute lg:block hidden z-[0] w-[92%] top-[55%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-[15%]"
           quality={60}
           width={900}
           height={900}
