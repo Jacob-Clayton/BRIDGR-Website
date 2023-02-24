38a332b2af0da4e4c8df3f1853633d6c2eb80fc5
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-24 10:38:01 +0000

    button hover colours

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='bg-[#1934A4] text-white py-2 px-6 items-center flex flex-row rounded duration-500'>
+    <button className='bg-[#1934A4] text-white hover:text-slate-300 py-2 px-6 items-center flex flex-row rounded duration-400'>
       {props.children} <img src="/external-link.svg" className="w-[20px] h-[20px] ml-2" />
     </button>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,43 +11,43 @@ function MobileNav({open, setOpen}) {
       <div className="z-100 flex flex-col items-center mt-[40%] sm:mt-[38%] md:mt-[35%] lg:mt-[25%]">
 
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="#Home" 
             onClick={() => setOpen(false)}>
             Home
           </a>
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="#About" 
             onClick={() => setOpen(false)}>
             About
           </a>
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="#Services" 
             onClick={() => setOpen(false)}>
             Services
           </a>
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="#Events" 
             onClick={() => setOpen(false)}>
             Events
           </a>
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="#Team" 
             onClick={() => setOpen(false)}>
             Team
           </a>
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="#Story" 
             onClick={() => setOpen(false)}>
             Origins
           </a>
           <a 
-            className="text-md sm:text-l md:text-xl font-bold my-3 text-white cursor-pointer" 
+            className="text-md sm:text-l md:text-xl font-bold my-3 text-white hover:text-slate-300 duration-400 cursor-pointer" 
             href="https://jacobclayton.gitbook.io/bridgr-services/" 
             onClick={() => setOpen(false)}>
             <Button>Docs</Button>
