021a019bc1fb40849c2695acf9ffb88f172fae38
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-25 22:35:32 +0000

    next/image hero + hero paddings

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,7 +16,13 @@ const Hero = () => {
     
     <section id="Home" className={` flex sm:flex-row flex-col mx-auto text-center sm:text-left pb-5 sm:pb-0 ${styles.innerWidth}`}>
 
-      <div className={`flex-2 ${styles.flexStart} flex-col mt-[28%] sm:mt-[17%] sm:mb-[4%] md:mt-[16%] md:mb-[1%] lg:mt-[10%] lg:mb-[0%] xl:mt-[12%] xl:mb-[6%] 2xl:mt-[16%] 2xl:mb-[10%] z-20`}>
+      <motion.div 
+        ariants={staggerContainer}
+        initial={isLgScreen ? 'hidden' : 'visible'}
+        whileInView="show"
+        viewport={{ once: false, amount: 0.2 }}
+        className={`flex-2 ${styles.flexStart} flex-col mt-[28%] sm:mt-[17%] sm:mb-[4%] md:mt-[16%] md:mb-[1%] lg:mt-[10%] lg:mb-[0%] xl:mt-[12%] xl:mb-[6%] 2xl:mt-[16%] 2xl:mb-[10%] z-20`}
+      >
         <div>
           <motion.h1 variants={textVariant(0.6)} className={`${styles.heroHeading} sm:px-16 px-6 flex-1`}>
             The Best of Web3
@@ -31,19 +37,32 @@ const Hero = () => {
             >
               A collective of Web3 companies working together to accelerate growth.
             </motion.p>
-            <div className={`${styles.xPaddings} sm:mt-8 md:mt-6 mt-5`}>
+            <motion.div 
+              variants={fadeIn('up', 'tween', 0.2, 1)}
+              className={`${styles.xPaddings} sm:mt-8 md:mt-6 mt-5`}
+            >
               <a className= "text-md sm:text-l md:text-xl font-bold text-white duration-400 cursor-pointer" href="https://t.me/bridgr">
-              <button className='bg-[#253fa8] hover:bg-[#1934A4] text-white py-2 px-6 items-center flex flex-row rounded duration-500'>
-                Join Us <img src="/telegram.svg" className="w-[20px] h-[20px] ml-4" />
-              </button>
+                <button className='bg-[#253fa8] hover:bg-[#1934A4] text-white py-2 px-6 items-center flex flex-row rounded duration-500'>
+                  Join Us <img src="/telegram.svg" className="w-[20px] h-[20px] ml-4" />
+                </button>
               </a>
-            </div>
+            </motion.div>
           </div>
-      </div>
+      </motion.div>
 
-      <div className={`flex-1 flex sm:justify-end justify-center sm:px-6 md:px-14 md:px-8 lg:px-10 xl:px-10 2xl:px-0 px-4 pb-20 sm:pb-0 items-center `}>
-        <img src='/cover.png' alt="cover" className="absolute w-[65%] sm:w-[50%] md:w-[50%] lg:w-[50%] xl:w-[48%] 2xl:w-[42%] z-0  opacity-[90%]" />
-      </div>
+      <motion.div 
+        className={`flex-1 flex sm:justify-end justify-center sm:px-6 sm:pt-20 md:px-14 md:px-8 md:pt-20 lg:px-10 lg:pt-20 xl:px-10 xl:pt-20 2xl:px-0 2xl:pt-20 px-4 pb-6 pt-12 sm:pb-0 sm:pb-0 items-center `}
+      >
+        <Image 
+          src='/cover.png' 
+          alt="cover" 
+          quality={95}
+          width={1600}
+          height={1000}
+          priority
+          className="absolute w-[65%] sm:w-[50%] md:w-[50%] lg:w-[50%] xl:w-[48%] 2xl:w-[40%] z-0 opacity-[90%]" 
+        />
+      </motion.div>
     {/* <div className=''>
       <div className='border-[1px] absolute'> */}
         {/* gradient start */}
