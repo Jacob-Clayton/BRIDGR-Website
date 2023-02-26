f2a4eb2c7990d324dad75f6d3580cd117accb24d
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-02-26 19:44:22 +0000

    tiny

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = (props) => {
   return (
     <button className='bg-[#1934A4] hover:bg-[#253fa8] text-white py-2 px-6 items-center flex flex-row rounded duration-500'>
-      {props.children} <img src="/external-link.svg" className="w-[20px] h-[20px] ml-2" />
+      {props.children} <img src="/external-link.svg" className="sm:w-[20px] sm:h-[20px] w-[16px] h-[16px] ml-2" />
     </button>
   );
 };
