1547af4f25b34f2cdf86b91b8cedd623ae6a77e4
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-03 14:54:05 +0000

    text

--- sections/Story.jsx
+++ sections/Story.jsx
@@ -38,7 +38,7 @@ const Story = () => {
             >
                 BRIDGR aims to build a thriving and collaborative Web3 ecosystem that is driven by the right motives. 
                 We believe that by bringing together a community of like-minded and well-established Web3 enthusiasts, 
-                we can drive innovation and collaboration towards a shared vision of a more equitable and decentralized future. 
+                we can drive innovation and collaboration towards a more equitable and decentralized future. 
                 Together, we are determined to build a better Web3 for all.
             </motion.p>
             {/* <div className='flex flex-row justify-center items-center'>
