683187670b0fea47cdb286f16fd9f55d3ae2c860
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-19 00:19:01 +0000

    tooltip wider for larger screens + hero fixed for 4k

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -70,7 +70,7 @@ const Hero = () => {
           width={1600}
           height={1000}
           priority
-          className="absolute w-[95%] 2xl:right-[17%] sm:w-[50%] md:w-[50%] lg:w-[50%] xl:w-[48%] 2xl:w-[38%] z-10 opacity-[90%]" 
+          className="absolute w-[95%] 2xl:right-[17%] sm:w-[50%] md:w-[50%] lg:w-[50%] xl:w-[48%] 2xl:w-[38%] max-w-[900px] z-10 opacity-[90%]" 
         />
         <div className="absolute z-[0] w-[40%] h-[50%] rounded-full gradient-01 opacity-[25%] lg:block hidden" />
       </div>
--- styles/index.js
+++ styles/index.js
@@ -1,5 +1,5 @@
 const styles = {
-  innerWidth: '2xl:max-w-[1100px] w-full',
+  innerWidth: '2xl:max-w-[1100px] xl:max-w-[1200px] w-full',
   interWidth: 'lg:w-[80%] w-[100%]',
 
   paddings: 'sm:p-16 xs:p-8 px-9 py-12',
