7015cef37ab5c93bd7ec0655c09882f61d3ba00e
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-20 18:37:31 +0000

    head

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ const Home = () => (
   <>
     <Head>
       <title>BRIDGR</title>
+      <meta property="og:title" content="BRIDGR" key="title" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
