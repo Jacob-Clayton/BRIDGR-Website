b6c9822d54f845d93404e0780ec797ab4c20604c
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-20 17:43:19 +0000

    pages/api/contact route changed

--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -7,7 +7,7 @@ import styles from '../styles';
 export default function Contact() {  
   
   return (
-    <section className={`${styles.paddings} relative z-10`} id='contact'>
+    <section className={`${styles.paddings} relative z-10`} id='Contact'>
       <TitleText title={<>Contact</>} textStyles="text-center mb-10" />
       <div className='flex flex-col lg:flex-row w-full items-center justify-around'>
         <ContactForm />
