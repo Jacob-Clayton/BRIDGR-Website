08a842cdbcd3adf240f36dce725f78dc51f6b72e
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-21 03:36:38 +0000

    info button change to border

--- components/InfoButton.jsx
+++ components/InfoButton.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const InfoButton = ({ href, children }) => {
     return (
         <Link href={href}>
-            <div className=" bg-gray-200 hover:bg-gray-300 text-black font-semibold text-center py-2 px-4 mb-4 rounded-lg shadow-xl">
+            <div className="border-[2px] hover:bg-gray-200 border-gray-200 text-black font-semibold text-center py-2 px-4 mb-4 rounded-lg">
                 {children}
             </div>
         </Link>
