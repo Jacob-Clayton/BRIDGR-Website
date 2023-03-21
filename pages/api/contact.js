10df1993069d36c6a5767f5de4961d696e08c776
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-21 04:24:46 +0000

    contact.js adjust

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,32 +1,29 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-const mailer = async (req, res) => {
-	const { name, email, message } = req.body
+export default async (req, res) => {
+  const { name, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS_PASSWORD
+    }
+  });
 
-	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
-		auth: {
-			user: process.env.EMAIL,
-			pass: process.env.EMAIL_PASS,
-		},
-	})
-
-	try {
-		await transporter.sendMail({
-			from: email,
-			to: 'jacobeclayton@gmail.com',
-			subject: `BRIDGR Contact form submission from ${name}`,
-			html: `	<h3>Contact form submission from ${name}</h3><br>
-        			<p><strong>Email: </strong> ${email}</p><br>
-        			<p><strong>Message: </strong> ${message}</p><br>
-    		`,
-		})
-	} catch (error) {
-		return res.status(500).json({ error: error.message || error.toString() })
-	}
-	return res.status(200).json({ error: '' })
-}
-
-export default mailer
+  try {
+    await transporter.sendMail({
+      from: email,
+      to: "example@gmail.com",
+      subject: `Contact form submission from ${name}`,
+      html: `<p>You have a contact form submission</p><br>
+        <p><strong>Email: </strong> ${email}</p><br>
+        <p><strong>Message: </strong> ${message}</p><br>
+      `
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
+  return res.status(200).json({ error: "" });
+};
