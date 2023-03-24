a36b8197a43ac07eaa47b92a66b1fe17ecf4b35f
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-24 15:10:50 +0000

    removed parser

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,24 @@ import { About, Story, Quotes, Hero, Team, Services, Events, News, Contact } fro
 
 const Home = () => (
   <>
-    <div className="bg-primary-skyBlue overflow-hidden">
-      <div className='bg-image'>
-        <Navbar />
-        <Hero/>
-      </div>
+    <div className="bg-image bg-primary-skyBlue overflow-hidden">
+      <Navbar />
+      <Hero/>
     </div>
     <div className="relative">
       <div className='bg-primary-lightBlue'>
         <About />
       </div>
       <div className='bg-primary-darkBlue'>
-        {/* <div className="gradient-03 z-0 overflow-visible lg:block hidden" /> */}
         <Story />
-      </div>
-      <div className='bg-primary-darkBlue'>
         <Team />
-      </div>
-      <div className='bg-primary-darkBlue'>
         <Quotes />
       </div>
       <div className='bg-primary-lightBlue'>
         <Services />
         <Events />
         <News /> 
-        {/* <Contact/> */}
+        <Contact/>
         <Footer />
       </div>
     </div>
