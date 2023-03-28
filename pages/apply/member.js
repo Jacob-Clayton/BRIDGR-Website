8067d8db83479974b843b7cf9b3b4c07c63985d9
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-03-28 18:18:52 +0100

    go back = pricing

--- pages/apply/member.js
+++ pages/apply/member.js
@@ -23,8 +23,8 @@ export default function member() {
 					<p className="mt-10 font-normal 2xl:text-[21px] xl:text-xl md:text-lg text-md text-center text-secondary-white">Register your interest in becoming a member and we'll get back to you to discuss!</p>
 					<MemberForm/>
 					<button className='mx-auto mt-20 border-[2px] border-white border-opacity-20 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-2 px-6 items-center flex flex-row rounded-2xl duration-500'>
-						<Link className= "text-sm sm:text-l md:text-xl font-medium hover:text-white duration-400 cursor-pointer " href="/">
-						Go Home</Link><Image src="/arrow.svg" className="sm:w-[20px] sm:h-[20px] w-[18px] h-[18px] ml-3" width={20} height={20} alt="email"/>
+						<Link className= "text-sm sm:text-l md:text-xl font-medium hover:text-white duration-400 cursor-pointer " href="/#Pricing">
+						Go Back</Link><Image src="/arrow.svg" className="sm:w-[20px] sm:h-[20px] w-[18px] h-[18px] ml-3" width={20} height={20} alt="email"/>
 					</button>
 				</div>
 			</div>
