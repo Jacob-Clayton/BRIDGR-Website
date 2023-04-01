4c291f5ab677daf93d36823581bd72af1cdd079d
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-01 14:18:33 +0100

    istanbul swap with nftnyc

--- sections/News.jsx
+++ sections/News.jsx
@@ -30,31 +30,6 @@ const News = () => {
 
                 <div className="relative flex sm:flex-row flex-col gap-4">
                         <Image
-                            src="/istanbul_dj.jpg"
-                            alt="istanbul event"
-                            className="mx-auto lg:w-[180px] lg:h-[180px] w-[150px] h-[150px] rounded-[32px] object-cover"
-                            width={180} height={180}
-                        />
-                        <div className="w-full flex justify-between items-center">
-                            <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
-                            <a href='https://medium.com/@bridgrxyz/web3-social-on-april-1st-in-istanbul-solidarity-donations-through-web3-b1839981ed9a'>
-                                <p className="mx-auto sm:mx-0 font-normal text-xl sm:text-xl md:text-2xl lg:text-2xl text-white">
-                                    Istanbul Keynote Panel</p>
-                            </a>
-                                <p className="mt-[16px] font-normal 2xl:text-[21px] xl:text-xl md:text-lg text-md text-secondary-white">
-                                    Panel to raise awareness on how Web3 technologies can be implemented during emergency situations like the February earthquake, with networking afterwards on 
-                                    Apr 1st 2023.</p>
-                            </div>
-                            <a href='https://medium.com/@bridgrxyz/web3-social-on-april-1st-in-istanbul-solidarity-donations-through-web3-b1839981ed9a'>
-                                <div className="lg:flex hidden items-center justify-center w-[60px] h-[60px] rounded-full bg-transparent border-[1px] border-white hover:bg-white hover:bg-opacity-10 duration-500">
-                                        <img src="/arrow.svg" alt="arrow" className="w-[40%] h-[40%] object-contain"/> 
-                                </div>
-                            </a>  
-                        </div>
-                    </div>
-
-                    <div className="relative flex sm:flex-row flex-col gap-4">
-                        <Image
                             src="/gamehaus.jpg"
                             alt="nft nyc event"
                             className="mx-auto lg:w-[180px] lg:h-[180px] w-[150px] h-[150px] rounded-[32px] object-cover"
@@ -71,6 +46,31 @@ const News = () => {
                                     Apr 12th 2023.</p>
                             </div>
                             <a href='https://www.eventbrite.sg/e/nftnyc-after-party-on-april-12th-gamehaus-new-york-tickets-605643825987'>
+                                <div className="lg:flex hidden items-center justify-center w-[60px] h-[60px] rounded-full bg-transparent border-[1px] border-white hover:bg-white hover:bg-opacity-10 duration-500">
+                                        <img src="/arrow.svg" alt="arrow" className="w-[40%] h-[40%] object-contain"/> 
+                                </div>
+                            </a>  
+                        </div>
+                    </div>
+
+                    <div className="relative flex sm:flex-row flex-col gap-4">
+                        <Image
+                            src="/istanbul_dj.jpg"
+                            alt="istanbul event"
+                            className="mx-auto lg:w-[180px] lg:h-[180px] w-[150px] h-[150px] rounded-[32px] object-cover"
+                            width={180} height={180}
+                        />
+                        <div className="w-full flex justify-between items-center">
+                            <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+                            <a href='https://medium.com/@bridgrxyz/web3-social-on-april-1st-in-istanbul-solidarity-donations-through-web3-b1839981ed9a'>
+                                <p className="mx-auto sm:mx-0 font-normal text-xl sm:text-xl md:text-2xl lg:text-2xl text-white">
+                                    Istanbul Keynote Panel</p>
+                            </a>
+                                <p className="mt-[16px] font-normal 2xl:text-[21px] xl:text-xl md:text-lg text-md text-secondary-white">
+                                    Panel to raise awareness on how Web3 technologies can be implemented during emergency situations like the February earthquake, with networking afterwards on 
+                                    Apr 1st 2023.</p>
+                            </div>
+                            <a href='https://medium.com/@bridgrxyz/web3-social-on-april-1st-in-istanbul-solidarity-donations-through-web3-b1839981ed9a'>
                                 <div className="lg:flex hidden items-center justify-center w-[60px] h-[60px] rounded-full bg-transparent border-[1px] border-white hover:bg-white hover:bg-opacity-10 duration-500">
                                         <img src="/arrow.svg" alt="arrow" className="w-[40%] h-[40%] object-contain"/> 
                                 </div>
