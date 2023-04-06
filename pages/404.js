be4c70179325cd57ec72b1548286ecde54957b20
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-06 13:44:42 +0100

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+'use client';
+import styles from '../styles';
+import { Navbar, TitleText } from '../components';
+import Link from 'next/link';
+
+
+const NotFoundPage = () => {
+    return (
+        <>
+        <Navbar />
+        <section className={`${styles.paddings} relative bg-primary-lightBlue min-h-screen`}>
+                <div className={`${styles.innerWidth} mx-auto mt-5 sm:mt-10 md:mt-20`}>
+                    <TitleText title={<>404: Page Not Found</>}textStyles="text-center" />
+                    <p className="mt-10 font-normal 2xl:text-[21px] xl:text-xl md:text-lg text-md text-center text-secondary-white">
+                        Sorry, there is nothing to see here
+					</p>
+                    <div className='mt-10 flex flex-row gap-6 justify-center'>
+                        <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-1 pl-6 pr-5 items-center rounded-lg duration-500'>
+                            <Link className= "font-normal xl:text-xl lg:text-lg md:text-md text-sm hover:text-white duration-400 cursor-pointer" href="/">
+                                Home
+                            </Link>
+                        </button>
+                        <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-1 pl-6 pr-5 items-center rounded-lg duration-500'>
+                            <Link className= "font-normal xl:text-xl lg:text-lg md:text-md text-sm hover:text-white duration-400 cursor-pointer" href="/#Contact">
+                                Contact Us
+                            </Link>
+                        </button>
+                    </div>
+                </div>
+        </section>
+        </>
+    );
+};
+
+export default NotFoundPage;
