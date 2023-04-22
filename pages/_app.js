2443b88aa27556d8ef6cc4e0b6748bf95c5f559c
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-22 11:45:39 +0100

    emil and video call added to contact section

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,22 @@ import { Montserrat, Poppins, Urbanist } from '@next/font/google'
 const montserrat = Montserrat({
 	subsets: ['latin'],
 	variable: '--font-montserrat',
+	display: 'swap',
 })
 
 const poppins = Poppins({
 	subsets: ['latin'],
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-	variable: '--font-poppins'
+	variable: '--font-poppins',
+	display: 'swap',
 })
 
 const urbanist = Urbanist({
 	subsets: ['latin'],
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 	variable: '--font-urbanist',
-  })
+	display: 'swap',
+})
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
