aa2107dbd75687d791a8959af29d6689dadcf7e9
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-22 13:47:25 +0100

    404 buttons px fixed + dark bg

--- pages/404.js
+++ pages/404.js
@@ -8,19 +8,19 @@ const NotFoundPage = () => {
     return (
         <>
         <Navbar />
-        <section className={`${styles.paddings} relative bg-primary-lightBlue min-h-screen`}>
+        <section className={`${styles.paddings} relative bg-primary-darkBlue min-h-screen`}>
                 <div className={`${styles.innerWidth} mx-auto mt-5 sm:mt-10 md:mt-20`}>
                     <TitleText title={<>404: Page Not Found</>}textStyles="text-center" />
                     <p className="mt-10 font-normal 2xl:text-[21px] xl:text-xl md:text-lg text-md text-center text-secondary-white">
                         Sorry, there is nothing to see here
 					</p>
                     <div className='mt-10 flex flex-row gap-6 justify-center'>
-                        <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-1 pl-6 pr-5 items-center rounded-lg duration-500'>
+                        <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-1 px-4 items-center rounded-lg duration-500'>
                             <Link className= "font-normal xl:text-xl lg:text-lg md:text-md text-sm hover:text-white duration-400 cursor-pointer" href="/">
                                 Home
                             </Link>
                         </button>
-                        <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-1 pl-6 pr-5 items-center rounded-lg duration-500'>
+                        <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-1 px-4 items-center rounded-lg duration-500'>
                             <Link className= "font-normal xl:text-xl lg:text-lg md:text-md text-sm hover:text-white duration-400 cursor-pointer" href="/#Contact">
                                 Contact Us
                             </Link>
