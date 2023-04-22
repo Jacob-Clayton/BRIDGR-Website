91de2547f1dd04c2eaf08b3d582d2d52df1e26d8
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-22 12:37:09 +0100

    follow button = twitter not telegram

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -38,15 +38,15 @@ const Hero = () => {
               We are a collective of Web3 companies offering our combined strength to help you grow.
             </p>
           </div>
-          <div className="flex flex-row justify-center sm:justify-start">
-            <div className='2xl:pl-0 px-6 sm:pr-8 pr-4 sm:mt-8 md:mt-10 mt-5'>
+          <div className="flex flex-row justify-center sm:justify-start gap-2">
+            <div className='2xl:pl-0 px-6 sm:pr-8 sm:mt-8 md:mt-10 mt-5'>
                 <button className="border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-2 px-6 items-center flex flex-row rounded-lg duration-500">
-                  <a className={`${poppins.variable} font-poppins text-sm sm:text-l md:text-xl font-normal hover:text-white duration-400 cursor-pointer`} href="https://t.me/bridgr">
-                    Join us</a><Image src="/telegram.svg" className="sm:w-[22px] sm:h-[22px] w-[18px] h-[18px] ml-3" width={20} height={20} alt="telegram"/>
+                  <a className={`${poppins.variable} font-poppins text-sm sm:text-l md:text-xl font-normal hover:text-white duration-400 cursor-pointer`} href="https://twitter.com/bridgrxyz">
+                    Follow</a><Image src="/twitter.svg" className="sm:w-[22px] sm:h-[22px] w-[18px] h-[18px] ml-3" width={20} height={20} alt="twitter"/>
                 </button>
             </div>
             <div className='sm:mt-8 md:mt-10 mt-5'>
-                <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-2 px-6 items-center flex flex-row rounded-lg duration-500'>
+                <button className='border-[1px] border-white border-opacity-30 shadow-md hover:bg-white hover:bg-opacity-10 text-off-white py-2 px-5 items-center flex flex-row rounded-lg duration-500'>
                   <a className={`${poppins.variable} font-poppins text-sm sm:text-l md:text-xl font-normal hover:text-white duration-400 cursor-pointer`} href="#Contact">
                     Contact</a><Image src="/email.svg" className="sm:w-[22px] sm:h-[22px] w-[18px] h-[18px] ml-3" width={20} height={20} alt="email"/>
                 </button>
