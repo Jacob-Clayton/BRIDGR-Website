0d12ca50aaa53ab833c3ed63441db3a780cb1648
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-24 21:40:54 +0100

    member bridgr apple white

--- pages/apply/member.jsx
+++ pages/apply/member.jsx
@@ -19,7 +19,7 @@ export default function member() {
 			<Navbar />
 			<div className={`${styles.paddings} relative bg-primary-darkBlue min-h-screen`}>
 				<div className={`${styles.innerWidth} flex flex-col mx-auto mt-5 sm:mt-10 md:mt-20`}>
-					<TitleText title={(<><span className={`${urbanist.variable} font-urbanist text-off-white font-extrabold`}>BRIDGR </span>Membership</>)} textStyles="text-center" />
+					<TitleText title={(<><span className={`${urbanist.variable} font-urbanist text-[#fafafa] font-extrabold`}>BRIDGR </span>Membership</>)} textStyles="text-center" />
 					<p className="mt-10 font-normal 2xl:text-[21px] xl:text-xl md:text-lg text-md text-center text-secondary-white">
 						Register your interest in becoming a member and we'll get back to you for a chat.
 					</p>
