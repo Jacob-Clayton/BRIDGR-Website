29093af4626fcbe255657288e65c6203d2ef4a14
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-26 19:01:15 +0100

    remove external link from button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='border-[1px] border-white border-opacity-30 text-off-white py-1 px-4 items-start flex flex-row shadow-md rounded-lg duration-500 xl:text-xl lg:text-lg md:text-md text-sm'>
-      {props.children} <img src="/external-link.svg" className="sm:w-[10px] sm:h-[10px] w-[9px] h-[9px] ml-[2px]" alt='external link'/>
+    <button className='border-[1px] border-white border-opacity-30 text-off-white py-1 px-4 flex mx-auto my-auto shadow-md rounded-md duration-500 xl:text-xl lg:text-lg md:text-md text-sm'>
+      {props.children}
     </button>
   );
 };
