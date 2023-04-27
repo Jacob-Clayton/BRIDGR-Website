4a074b02c17f05ad80fc17a82fc2fb721262c696
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-04-27 16:34:44 +0100

    smaller newsletter input + success message below

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -50,16 +50,16 @@ export default function Newsletter() {
                 <div className="sm:text-left text-center mb-1">
                     <p className="text-md text-secondary-white">Subscribe for updates</p>
                 </div>
-                <div className="relative flex flex-row  flex-shrink-0 shadow-md rounded-md">
+                <div className="relative flex flex-row flex-shrink-0 shadow rounded-md">
                     <div className="flex flex-row sm:w-[370px] w-full">
-                        <input {...register("email")} required aria-required={true} type='email' placeholder="example@email.com" className="w-4/6 px-3 py-2 rounded-l-md sm:w-2/3"/>
-                        <button role="button" className="w-2/6 px-3 py-2 font-normal lg:text-lg md:text-md text-sm rounded-r-md sm:w-1/3 border border-white border-opacity-30 text-off-white">
+                        <input {...register("email")} required aria-required={true} type='email' placeholder="example@email.com" className="w-4/6 px-3 py-1 rounded-l-md sm:w-2/3"/>
+                        <button role="button" className="w-2/6 px-3 py-1 font-normal lg:text-lg md:text-md text-sm rounded-r-md sm:w-1/3 border border-white border-opacity-30 text-off-white">
                             {isSubmitting ? "Submitting" : "Subscribe"}
                         </button>
                         
                     </div>
                 </div>
-                <span className='flex items-center justify-center'>
+                <span className='flex items-center justify-center mt-2'>
                     {form.state === 'loading' && <LoadingSpinner />}
                     {form.state === 'error' ? (
                         <ErrorMessage>{form.message}</ErrorMessage>
