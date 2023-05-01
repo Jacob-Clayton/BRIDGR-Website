22653828907aae1f8c3cc8e0bd1a4f87e24b92b9
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-05-01 14:13:06 +0100

    team dtext + gap instead of mt for socials

--- sections/Team.jsx
+++ sections/Team.jsx
@@ -45,19 +45,19 @@ const Team = () => {
             initial={isLgScreen ? 'hidden' : 'visible'}
             whileInView="show"
             viewport={{ once: false, amount: 0.01 }}
-            className="flex flex-wrap justify-center -m-2 md:mt-10 mt-5"
+            className={`${styles.descriptionText} flex flex-wrap justify-center -m-2 md:mt-10 mt-5`}
           >
 
             <div className="p-2 xl:w-1/3 lg:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/mark.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Mark </p>
-                  <p className="">Co-founder</p>
+                  <p className={``}>Mark </p>
+                  <p className="text-secondary-white">Co-founder</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/mepusica"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/mepusica"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/mepusica"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
@@ -66,12 +66,12 @@ const Team = () => {
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/kishor.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Kishor</p>
-                  <p className="">Co-founder</p>
+                  <p className={``}>Kishor</p>
+                  <p className="text-secondary-white">Co-founder</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/madforcryptos_"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/kishormm/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/kishormm/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
@@ -80,12 +80,12 @@ const Team = () => {
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/jacob.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Jacob Clayton</p>
-                  <p className="">Co-founder</p>
+                  <p className={``}>Jacob Clayton</p>
+                  <p className="text-secondary-white">Co-founder</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/kolsas_eth"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/jebclayton/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/jebclayton/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
@@ -94,12 +94,12 @@ const Team = () => {
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/sima.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Sima Baktas</p>
-                  <p className="">Crypto Laywer & Founder of GlobalB</p>
+                  <p className={``}>Sima Baktas</p>
+                  <p className="text-secondary-white">Crypto Laywer & Founder of GlobalB</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/simabaktas"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/simabaktas/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/simabaktas/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
@@ -108,12 +108,12 @@ const Team = () => {
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/hilal.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Hilal Baktas</p>
-                  <p className="">Founder of CryptoFemale & GlobalB</p>
+                  <p className={``}>Hilal Baktas</p>
+                  <p className="text-secondary-white">Founder of CryptoFemale & GlobalB</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/hilal_baktas"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/hilalbaktas/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/hilalbaktas/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
@@ -122,12 +122,12 @@ const Team = () => {
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/henry.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Henry Foo</p>
-                  <p className="">Founder of Ethlas & Web3 Software Engineer</p>
+                  <p className={``}>Henry Foo</p>
+                  <p className="text-secondary-white">Founder of Ethlas & Web3 Software Engineer</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/henry_ethlas"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/henry-foo/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/henry-foo/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
@@ -136,12 +136,12 @@ const Team = () => {
               <div className="h-full flex items-center border-gray-500 border-opacity-30 border p-4 rounded">
                 <Image alt="team" width={150} height={150} className="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="/jayefoo.png"/>
                 <div className="flex-grow">
-                  <p className={`${styles.descriptionText}`}>Jaye Foo</p>
-                  <p className="">Founder of Bored Island Group, Parlour Global & Bighaus</p>
+                  <p className={``}>Jaye Foo</p>
+                  <p className="text-secondary-white">Founder of Bored Island Group, Parlour Global & Bighaus</p>
                 </div>
-                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2">
+                <div className="flex flex-col flex-shrink-0 ml-2 xl:mr-4 mr-2 gap-3">
                   <a href="https://twitter.com/jayefunk"><img src="/twitter.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='twitter'></img></a>
-                  <a href="https://www.linkedin.com/in/jayefoo/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white mt-2" alt='linkedin'></img></a>
+                  <a href="https://www.linkedin.com/in/jayefoo/"><img src="/linkedin.svg" className="w-[16px] h-[16px] sm:w-[20px] sm:h-[20px] md:w-[16px] md:h-[16px] lg:w-[17px] lg:h-[17px] xl:w-[18px] xl:h-[18px] object-contain cursor-pointer text-white" alt='linkedin'></img></a>
                 </div>
               </div>
             </div>
