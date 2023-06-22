4416182ab4c2ab386d078416922422ad0cca238b
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-06-22 12:42:17 +0100

    pricing table points reduced space

--- components/PricingTable.jsx
+++ components/PricingTable.jsx
@@ -75,7 +75,7 @@ const PricingTable = () => {
                                             </span>
                                         </div>
 
-                                        <ul className="flex flex-col mt-8 space-y-4">
+                                        <ul className="flex flex-col mt-8 space-y-2">
                                             <li className="inline-flex items-center space-x-2">
                                                 <svg className="flex-shrink-0 w-5 h-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                                     <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
