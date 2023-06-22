8bb0b0a4bba75f954c63ca28a39e13c454903fe4
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-06-22 12:39:29 +0100

    Pricing table responsive design

--- components/PricingTable.jsx
+++ components/PricingTable.jsx
@@ -15,12 +15,12 @@ const PricingTable = () => {
                 whileInView="show"
                 className={` mx-auto`}
             >
-                <div className="flex flex-wrap justify-center items-stretch">
+                <div className="flex flex-wrap">
 
-                    <div className="mx-auto">
-                        <div className="grid grid-cols-1 md:items-stretch md:grid-cols-2 gap-x-12 lg:gap-x-20 gap-y-10">
-                            <div className="flex flex-col justify-start lg:py-8">
-                                <h2 className={`${styles.headerText} lg:w-3/4`}>Join our global web3 events series</h2>
+                    <div className="mx-auto w-full">
+                        <div className="flex flex-col lg:flex-row lg:gap-x-2 gap-y-8">
+                            <div className="lg:w-1/2 2xl:w-2/3 flex flex-col justify-start lg:py-8">
+                                <h2 className={`${styles.headerText} lg:w-3/4 2xl:w-1/2`}>Join our global web3 events series</h2>
 
                                 <div className="mt-10">
                                     <div className="flex items-center">
@@ -52,7 +52,7 @@ const PricingTable = () => {
                                     </div>
 
                                     <blockquote className="mt-2">
-                                        <p className={`${styles.descriptionText}`}>Bridgr made it so simple. My company got exposure at events all around the world. I just joined Bridgr and they took care of the rest.</p>
+                                        <p className={`${styles.descriptionText} lg:w-3/4`}>Bridgr made it so simple. My company got exposure at events all around the world. I just joined Bridgr and they took care of the rest.</p>
                                     </blockquote>
 
                                     <div className="flex items-center mt-6">
@@ -65,7 +65,7 @@ const PricingTable = () => {
                                 </div>
                             </div>
 
-                            <div className="">
+                            <div className="lg:w-1/2 2xl:w-1/3">
                                 <div className="overflow-hidden bg-off-white rounded">
                                     <div className="p-10">
                                         <div className="space-y-2">
@@ -116,87 +116,6 @@ const PricingTable = () => {
                             </div>
                         </div>
                     </div>
-
-
-                    {/* <div className="flex w-full mb-8 sm:px-4 lg:w-1/2 2xl:w-1/3 lg:mb-0">
-                        <div className="flex flex-col p-6 space-y-6 rounded sm:p-8 bg-off-white">
-                            <div className="space-y-2">
-                                <p className={`${styles.subheaderText} text-lavender`}>Friend</p>
-                                <span className="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-4xl sm:text-5xl text-4xl font-bold text-cetecean-blue">$200
-                                    <span className="sm:text-sm sm:leading[20px] text-[13px] leading-[18px] font-medium tracking-wide ml-1">/ lifetime</span>
-                                </span>
-                            </div>
-                            <p className={`${styles.descriptionText} text-primary-black`}>Global events and media package with engagement from our shared community.&nbsp;
-                                <Link href="https://docs.bridgr.xyz/benefits/for-friends" className='font-medium hover-underline-animation-b'>Read more.</Link>
-                            </p>
-                            <ul className={`${styles.descriptionText} text-primary-black lg:space-y-[0.2rem] space-y-[0.1rem] flex-1`}>
-                                <li className="flex items-center space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Visibility</p>
-                                </li>
-                            </ul>
-                            <Link rel="noopener noreferrer" href="/contact" className={` ${styles.descriptionText} inline-block w-full px-5 py-3 text-center rounded bg-violet`}>
-                                Contact Us
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="w-full mb-8 sm:px-4 lg:w-1/2 2xl:w-1/3 lg:mb-0">
-                        <div className="p-6 space-y-6 rounded sm:p-8 bg-off-white">
-                            <div className="space-y-2">
-                                <p className={`${styles.subheaderText} text-lavender`}>Member</p>
-                                <span className="2xl:text-6xl xl:text-5xl lg:text-4xl md:text-4xl sm:text-5xl text-4xl font-bold text-cetecean-blue">$2000
-                                    <span className="sm:text-sm sm:leading[20px] text-[13px] leading-[18px] font-medium tracking-wide ml-1">/ year</span>
-                                </span>
-                            </div>
-                            <p className={`${styles.descriptionText} text-primary-black`}>Everything from Friend included, plus access to our partners' combined services.&nbsp;
-                                <Link href="https://docs.bridgr.xyz/benefits/for-members" className='font-medium hover-underline-animation-b'>Read more.</Link>
-                            </p>
-                            <ul className={`${styles.descriptionText} text-primary-black lg:space-y-[0.1rem] space-y-[0.05rem]`}>
-                                <li className="flex items-start space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Visibility</p>
-                                </li>
-                                <li className="flex items-start space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Business Building</p>
-                                </li>
-                                <li className="flex items-start space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Access Investors</p>
-                                </li>
-                                <li className="flex items-start space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Web3 Education</p>
-                                </li>
-                                <li className="flex items-start space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Web3 Legal Services</p>
-                                </li>
-                                <li className="flex items-start space-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#180D4C" className="flex-shrink-0 w-6 h-6">
-                                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                                    </svg>
-                                    <p>Web3 Development</p>
-                                </li>
-                            </ul>
-                            <Link rel="noopener noreferrer" href="/apply/member" className={` ${styles.descriptionText} inline-block w-full px-5 py-3 text-center rounded bg-violet`}>
-                                Register Interest
-                            </Link>
-                        </div>
-                    </div> */}
                     
                 </div>
             </motion.div>
